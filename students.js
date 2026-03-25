1fb8c27006832821964031e90fc5635822532433
chikontaiii <cyntemirkurmanbekov53@gmail.com>
2026-03-26 02:30:02 +0600

    вввв

--- students.js
+++ students.js
@@ -6,15 +6,16 @@ const tbody = document.getElementById('homework-body');
 
 async function loadHomework() {
     const snapshot = await getDocs(collection(db, "homework"));
-    tbody.innerHTML = ''; // очистим, если были тестовые строки
+    tbody.innerHTML = ''; // очищаем
+
     snapshot.forEach(doc => {
         const hw = doc.data();
+        const deadlineClass = hw.deadline < new Date().toISOString().split('T')[0] ? 'urgent' : '';
         const row = document.createElement('tr');
         row.innerHTML = `
-            <td class="homework-subject">${hw.subject}</td>
-            <td>${hw.task}</td>
-            <td class="homework-deadline ${hw.deadline < new Date().toISOString().split('T')[0] ? 'urgent' : ''}">${hw.deadline}</td>
-
+            <td data-label="Предмет" class="homework-subject">${hw.subject || ''}</td>
+            <td data-label="Задание">${hw.task || ''}</td>
+            <td data-label="Дедлайн" class="homework-deadline ${deadlineClass}">${hw.deadline || ''}</td>
         `;
         tbody.appendChild(row);
     });
